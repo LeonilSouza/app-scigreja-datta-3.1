f6fca167bc814f7234e8a3695c270f1626432fe6
Leonil Souza <leonilsouza@gmail.com>
2025-08-30 16:57:20 -0400

    ApiConfig

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -23,7 +23,7 @@ export class ThemeConfig {
 
 export const API_CONFIG = {
   // baseUrl: "https://api-scigreja-4fbe1bc9e551.herokuapp.com" 
-  // baseUrl: "http://localhost:8080"
+  baseUrl: "http://localhost:8080"
 }
 
 export let GLOBALS: any = {
